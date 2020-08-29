73ffca7b221d4b8f514e5b4c5a722cbe6880202f
iagpomponet <iagosilbernagel@hotmail.com>
2020-08-29 09:45:02 -0300

    feat: header

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import HomePage from './pages/home/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
+
 
 import './App.css'
 
@@ -10,6 +12,7 @@ import { Route, Switch } from 'react-router-dom';
 function App() {
   return <div>
     {/* switch nao renderiza nada depois de achar uma url que bata */}
+    <Header path="/"/>
     <Switch>
       <Route exact path='/' component={HomePage} />
       <Route exact path='/shop' component={ShopPage} />
