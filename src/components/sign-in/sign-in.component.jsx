daab96eb136f754d00b23e9e960973f68586fe1e
iagpomponet <iagosilbernagel@hotmail.com>
2020-08-31 20:36:19 -0300

    feat: sign in with e-mail and password

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -4,7 +4,7 @@ import './sign-in.styles.scss';
 
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../customButton/custom-button.component';
-import { signInWithGoogle } from '../../firebase/firebase.utils';
+import { auth, signInWithGoogle } from '../../firebase/firebase.utils';
 
 class SignIn extends React.Component {
   constructor(){
@@ -16,8 +16,19 @@ class SignIn extends React.Component {
     }
   }
 
-  handleSubmit = (event) =>{
+  handleSubmit = async (event) =>{
     event.preventDefault();
+
+    const { email, password } = this.state;
+
+    try{
+      await auth.signInWithEmailAndPassword(email, password);
+
+      this.setState({ email : '', password: ''});
+    }
+    catch(error){
+      console.log(error);
+    }
 
     this.setState({
       'email': '',
@@ -41,7 +52,7 @@ class SignIn extends React.Component {
 
         <form onSubmit={this.handleSubmit}>
           <FormInput type="email" name="email" label='name' required value={this.state.email} handleChange={this.handleChange} />
-          <FormInput type="password" name="password" label='password' required value={this.state.email} handleChange={this.handleChange} />
+          <FormInput type="password" name="password" label='password' required value={this.state.password} handleChange={this.handleChange} />
 
 
           <div className="buttons">
